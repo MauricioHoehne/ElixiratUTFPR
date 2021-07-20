803b8013077be048ec2501b8e815a3078d7ecf51
MauricioHoehne <87049827+MauricioHoehne@users.noreply.github.com>
2021-07-19 22:31:46 -0300

    second version of the test

--- test/advent1_test.exs
+++ test/advent1_test.exs
@@ -2,7 +2,8 @@ defmodule Advent1Test do
   use ExUnit.Case
   doctest Advent1
 
-  test "greets the world" do
-    assert Advent1.hello() == :world
+  test "Test the load file and calculate challenge" do
+    fileInput = "C:\\Users\\mauri\\OneDrive\\Utfpr\\Funct_Prog\\advent1\\data\\AdventofCodeChallengeInput.txt"
+    assert Advent1.readAdventFile(fileInput) == 1018944
   end
 end
